8c34ed3ccde3966e98d565b887e5d6e851a36c3e
vanet project <vanetprojectbe@gmail.com>
2026-03-19 17:22:52 +0530

    Update sensors.h

--- OBU/sensors/sensors.h
+++ OBU/sensors/sensors.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <Arduino.h>
+
+// MPU6050
 float read_acceleration();
+
+// MPU9250
+float read_acc2();
 float read_gyro();
+
+// Other sensors
 float read_vibration();
 float read_temperature();
